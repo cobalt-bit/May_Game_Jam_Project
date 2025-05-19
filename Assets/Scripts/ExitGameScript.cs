4afe591307a2a42d1da160ddd069bd7555170cf5
ひるねこ <coblt255@gmail.com>
2025-05-19 23:00:08 +0900

    タイトル画面UIの修正
    ボタンが反応しないバグを修正しました。

--- Assets/Scripts/ExitGameScript.cs
+++ Assets/Scripts/ExitGameScript.cs
@@ -1,3 +1,4 @@
+using UnityEditor;
 using UnityEngine;
 
 public class ExitGameScript : MonoBehaviour
@@ -5,7 +6,7 @@ public class ExitGameScript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        DontDestroyOnLoad(gameObject);
     }
 
     // Update is called once per frame
@@ -16,6 +17,11 @@ public class ExitGameScript : MonoBehaviour
 
     public void ExitGame()
     {
+        Debug.Log("Exit Game");
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
         Application.Quit();
+#endif
     }
 }
